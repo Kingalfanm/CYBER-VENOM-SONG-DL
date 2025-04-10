0122f9cf2fa6ca16da6509bc07b237f2e739d464
DarkSahi75 <darksahi75@gmail.com>
2025-04-10 19:11:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=1PdyzCyA#esET0w5MBctTABwOTHDW4cy28UOOOABK86fyUgRYxLY',
+SESSION_ID: process.env.SESSION_ID || 'SAHAS-MD=1PdyzCyA#esET0w5MBctTABwOTHDW4cy28UOOOABK86fyUgRYxLY',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 OWNER_NUMBER: process.env.OWNER_NUMBER || '94765527900', 
 PREFIX:  process.env.PREFIX || ['.'] ,
