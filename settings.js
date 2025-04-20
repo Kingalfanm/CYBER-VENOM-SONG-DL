722238ba6c3cdbe03e1583a6bdef4149a847fb17
Kingalfanm <153409894+Kingalfanm@users.noreply.github.com>
2025-04-20 19:06:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'SAHAS-MD=1PdyzCyA#esET0w5MBctTABwOTHDW4cy28UOOOABK86fyUgRYxLY',
+SESSION_ID: process.env.SESSION_ID || 'SAHAS-MD=H0IhwZCD#P0oMIls2go2RgHtQWizqfHXmnpn5cnJG2kDewvmeJmk',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
-OWNER_NUMBER: process.env.OWNER_NUMBER || '94765527900', 
+OWNER_NUMBER: process.env.OWNER_NUMBER || '94740433291', 
 PREFIX:  process.env.PREFIX || ['.'] ,
-JID: process.env.JID || "120363287634683059@newsletter",
+JID: process.env.JID || "120363400534364543@newsletter",
 WACHLINK: process.env.WACHLINK || `https://whatsapp.com/channel/0029Vb8WOgSBPzjfG09WEn10`,
-FOOTER: '> *©ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀʜᴀꜱ & ᴅɪɴᴜᴡᴀ*',
+FOOTER: '> ➤ 𝚂𝙷𝙰𝙶𝙴𝙴 𝚂𝙾𝙽𝙶 𝙷𝚄𝙱🔐💝',
 DIRECTION: true,
 LOGO: process.env.LOGO || `https://files.catbox.moe/siaqr5.jpg`     
 };
